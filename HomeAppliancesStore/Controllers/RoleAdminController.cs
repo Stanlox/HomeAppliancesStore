5d760ceec5d03c29296844f6f82d9bf50d5124b1
Stanlox <maxim.bondaruk2000@mail.ru>
2020-07-28 15:39:06 +0300

    Added the missing controller.

---
+++ HomeAppliancesStore/Controllers/RoleAdminController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeAppliancesStore.Controllers
+{
+    public class RoleAdminController : Controller
+    {
+        private RoleManager<IdentityRole> roleManager;
+
+        public RoleAdminController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        public ViewResult Index()
+        {
+            return View(this.roleManager.Roles);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Required]string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                else
+                {
+                    ModelState.AddModelError("", result.Errors.ToString());
+                }
+            }
+
+            return View(name);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+
+            if(role != null)
+            {
+                IdentityResult result = await roleManager.DeleteAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("", result.Errors.ToString());
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Role no found");
+            }
+
+            return View("Index", roleManager.Roles);
+        }
+    }
+}
